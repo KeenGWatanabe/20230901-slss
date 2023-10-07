fc5b5d0faba842aa75567b0e553ff4f085f452c6
RgerWeb <mckeenweb@gmail.com>
2023-10-07 16:45:44 +0800

    fw

--- chat.js
+++ chat.js
@@ -10,15 +10,26 @@ function lky() {
 	document.getElementById("answer").innerHTML="Your input is blank";      
 	}else if(inputText.toLowerCase().indexOf("thank you")>-1){
 			document.getElementById("answer").innerHTML="You are welcome.不用客气。";
-	}//school*
+	}//s2-hygiene*
+    else if(inputText.toLowerCase().indexOf("disease")>-1&&inputText.toLowerCase().indexOf("transmi")>-1||inputText.toLowerCase().indexOf("disease")>-1&&inputText.toLowerCase().indexOf("transfer")>-1){document.getElementById("answer").innerHTML=`some transmissable diseases in the cause of lifesaving to be aware of are: colds, influenza, measles and mumps, glandular fever, Hepatitis A or B, AIDS, SARS, Herpes, Tuberculosis, some forms of meningitis, some skin infections.SLSS manual`;}
+//s2-hygiene***
+//s3-rescue*
+else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.SLSS manual`;}
+//s3-rescue***
+//s4-resuscitation*
+else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.SLSS manual`;}
+//s4-resuscitation***
+//s5-emergency care*
+else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.SLSS manual`;}
+//s5-emergency care***
+//s6-anatomy & physiology*
+else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.SLSS manual`;}
+//s6-anatomy & physiology***
 
-else if(inputText.toLowerCase().indexOf("chinese")>-1&&inputText.toLowerCase().indexOf("speaking")>-1||inputText.toLowerCase().indexOf("chinese")>-1&&inputText.toLowerCase().indexOf("immigrants")>-1){document.getElementById("answer").innerHTML=`The remainder were the Chinese-speaking Chinese who had come to Singapore more recently. They spoke not English but their own dialects-mainly Hokkien,Teochew,Cantonese,Hakka and Hainanese. Their children went to Chinese schools, where they learnt Mandarin. Their contact with the British authorities was minimal,||Their loyalty was to China, not Britain .Memoirs of LKY,1998`;}
-else if(inputText.toLowerCase().indexOf("mpaja")>-1||inputText.toLowerCase().indexOf("communist")>-1&&inputText.toLowerCase().indexOf("army")>-1||inputText.toLowerCase().indexOf("guerrilla")>-1||inputText.toLowerCase().indexOf("japanese")>-1&&inputText.toLowerCase().indexOf("anti")>-1){document.getElementById("answer").innerHTML=`It was they who went into the Malayan jungle to fight the Japanese, as guerrillas in the Malayan People's Anti-Japanese Army (MPAJA), the military arm of the Malayan Communist Party. They were looking ahead to the day when they would expel not only the Japanese, but also the British. In the power vacuum created when the Japanese surrendered suddenly before the British could invade, they spelt trouble. In Malaya, they took over some of the smaller towns, put up arches to welcome the guerrillas as the real victors in the battle against the Japanese, and acted as the de facto local authority.Memoirs of LKY,1998`;}
-else if(inputText.toLowerCase().indexOf("communist")>-1&&inputText.toLowerCase().indexOf("law")>-1||inputText.toLowerCase().indexOf("communist")>-1&&inputText.toLowerCase().indexOf("turmoil")>-1){document.getElementById("answer").innerHTML=`In one instance 20 Chinese detectives were rounded up and put into pig crates pending trial. There was extortion and subtle blackmail of businessmen for their past collaboration with the enemy. Many prominent people were psychologically or physically compelled to make generous contributions to the MPAJA to make up for their past misdeeds. Young hooligans went around town openly using MPAJA credentials to wring money or goods from those who had had dealings with the Japanese. The British forces could not reestablish law and order in the face of the MPAJA's aggressiveness and the opportunism of gangsters||. Fortunately, because they had no means to travel down to Singapore, most of the MPAJA remained stuck in Malaya.Memoirs of LKY,1998`;}
-
-//after liberation-singapore***
-		//page 33 Sample codes*	
-//else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.Memoirs of LKY,1998`;}
+//s7-legal aspects*
+else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.SLSS manual`;}
+//s7-legal aspects***
+//else if(inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=`.SLSS manual`;}
 	
 //	document.getElementById("answer").innerHTML=`<a href="">mt</a>`;}
 //else if(/\bx\b/.test(inputText.toLowerCase())&&inputText.toLowerCase().indexOf("x")>-1){document.getElementById("answer").innerHTML=`Memoirs of LKY,1998`;}
