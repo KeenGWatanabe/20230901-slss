578c3b79ecf96ccbd6562f0b725ec820fdea6f88
RgerWeb <mckeenweb@gmail.com>
2023-10-12 06:48:47 +0800

    A

--- list/list1.js
+++ list/list1.js
@@ -12,5 +12,27 @@ function displayAnswer(elementId) {
     alert(`calm cooperative clothed casualty; hold casualty cloths below collar to avoid choking neck, arms relax avoid jerking motion; egg beater kick or survival kick; encourage casualty to scull with free arm and kick if possible, maintain observation of casualty. (Reference from SLSS manual)`);
     
 } else if (elementId === 'wrist tow') {
-    alert(`calm cooperative casualty;<br>grasp back of casualty's wrist;side stroke scissors kick;encourage casualty to stroke with free arm and kick if possiblemaintain observation of casualty.. (Reference from SLSS manual)`);
+    alert(`calm cooperative casualty;grasp back of casualty's wrist;side stroke scissors kick;encourage casualty to stroke with free arm and kick if possiblemaintain observation of casualty.. (Reference from SLSS manual)`);
+
+} else if (elementId === 'chin tow') {
+    alert(`necessaary to maintain firm, close control of casualty;approach from behind, place one hand on casualty chin;rest casualty's head on your shoulder on towing arm side;survival kick, egg beater kick.(Reference from SLSS manual)`);
+
+} else if (elementId === 'double shoulder tow') {
+    alert(`necessaary to maintain greater control, elevate casualty head over rough water;approach from behind, hook both hands under the armpits and grasp the shoulders;survival kick, egg beater kick.(Reference from SLSS manual)`);
+
+} else if (elementId === 'cross chest tow') {
+    alert(`over rough water;approach from behind, cross an arm over the chest of casualty;grip under the armpit clamp elbow to casualty's chest;sidestroke scissors kick.(Reference from SLSS manual)`);
+
+} else if (elementId === 'support tow') {
+    alert(`with buoyant aid for unconscious non-breathing casualty;hold board with one hand, chin tow casualty;every 3 sec, EAR blow to the nose(rest head on board elbow, chin hand press thumb to seal the mouth).CHECK THIS .(Reference from SLSS manual)`);
+
+} else if (elementId === 'head tow') {
+    alert(`firm stabilize head hold necessary;palm inwards place firmly both side of casualty's head;survial kick or egg beater kick.(Reference from SLSS manual)`);
+
+} else if (elementId === 'vice grip tow') {
+    alert(`for suspected spinal injury;approach from side;place one forearm along spine with hand cupping back of casualty's head;other forearm on casualty's sternum supporting jaw;lock both writs and squeeze the forearms together to create a vice, immobilises casualty's body with an upward downward pressure;for casualty face down in water, vice grip first then rotate person to face up; move carefully minimise motion altering casualty's hips and legs.(Reference from SLSS manual)`);
+
+} else if (elementId === '') {
+    alert(``);
+
 }}
