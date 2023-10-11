5777ee2b966854b935bbe00f6a9f65e6b84ade37
RgerWeb <mckeenweb@gmail.com>
2023-10-11 08:59:44 +0800

    g

--- list.js
+++ list.js
@@ -6,15 +6,19 @@ function displayAnswer(elementId) {
     S-ervice to society, 
     C-oncern for safety, 
     U-nited in our mission, 
-    E-njoyment in what we do`);
+    E-njoyment in what we do
+    .(SLSS manual)`);
 } else if (elementId === 'mission') {
-    alert(`to promote lifesaving and water safety`);
+    alert(`to promote lifesaving and water safety. (SLSS)`);
 }
  else if (elementId === 'vision') {
-    alert(`to be a leading lifesaving organisation in the world`);
+    alert(`to be a leading lifesaving organisation in the world. (SLSS)`);
 }
 else if (elementId === 'contents') {
     window.open(href="list/contents.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 'disclaimer') {
+    alert(`This content is designed for trainee and trained lifesaver's references only.  Context of saving methodology is subject to skills and experiences interpretation, quotes here are by no means comprehensive enough to be used alone. (Web Developer)`);
 }
 else if (elementId === 's3Rescue') {
     window.open(href="list/s3Rescue.html",'chat AI','resizable,height=300,width=500');
@@ -31,7 +35,5 @@ else if (elementId === 's8Resuscitation') {
 else if (elementId === 's3Tow') {
     window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === 'accompanied rescue') {
-    alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp aid firmly, kick to move;providing reassurance throughout(SLSS manual)`);
-}
+
 }
--- list/list1.js
+++ list/list1.js
@@ -2,11 +2,11 @@ function displayAnswer(elementId) {
     if (elementId === 'accompanied rescue') {
     alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp aid firmly, kick to move;providing reassurance throughout(SLSS manual)`);
 
-} else if (elementId === 'mission') {
-    alert(`to promote lifesaving and water safety`);
+} else if (elementId === 'non-contact tow') {
+    alert(`swim out with a buoyant aid. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp kickboard using hugging method firmly and float on their back, providing reassurance throughout, hold edge of kickboad and tow casualy prefrebaly with sidestroke scissors kick.(SLSS manual)`);
 
-} else if (elementId === 'vision') {
-    alert(`to be a leading lifesaving organisation in the world`);
+} else if (elementId === 'extended tow') {
+    alert(`calm cooperative casualty; hold casualty's chin, elbows locked straight; side-stroke scissors kick; maintain observation of casualty. (SLSS manual)`);
     
 } else if (elementId === 's3Tow') {
     window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
