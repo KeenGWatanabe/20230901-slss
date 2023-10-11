3abfc0a9d0753431fd3550b8a34dc6aa57685739
RgerWeb <mckeenweb@gmail.com>
2023-10-11 17:12:28 +0800

    t

--- list/list1.js
+++ list/list1.js
@@ -1,15 +1,16 @@
 function displayAnswer(elementId) {
     if (elementId === 'accompanied rescue') {
-    alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp aid firmly, kick to move;providing reassurance throughout(SLSS manual)`);
+    alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp aid firmly, kick to move;providing reassurance throughout(Reference from SLSS manual)`);
 
 } else if (elementId === 'non-contact tow') {
-    alert(`swim out with a buoyant aid. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp kickboard using hugging method firmly and float on their back, providing reassurance throughout, hold edge of kickboad and tow casualy prefrebaly with sidestroke scissors kick.(SLSS manual)`);
+    alert(`swim out with a buoyant aid. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp kickboard using hugging method firmly and float on their back, providing reassurance throughout, hold edge of kickboad and tow casualy prefrebaly with sidestroke scissors kick.(Reference from SLSS manual)`);
 
 } else if (elementId === 'extended tow') {
-    alert(`calm cooperative casualty; hold casualty's chin, elbows locked straight; side-stroke scissors kick; maintain observation of casualty. (SLSS manual)`);
+    alert(`calm cooperative casualty; hold casualty's chin, elbows locked straight; side-stroke scissors kick; maintain observation of casualty. (Reference from SLSS manual)`);
     
-} else if (elementId === 's3Tow') {
-    window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
-}
-
-}
+} else if (elementId === 'clothing tow') {
+    alert(`calm cooperative clothed casualty; hold casualty cloths below collar to avoid choking neck, arms relax avoid jerking motion; egg beater kick or survival kick; encourage casualty to scull with free arm and kick if possible, maintain observation of casualty. (Reference from SLSS manual)`);
+    
+} else if (elementId === 'wrist tow') {
+    alert(`calm cooperative casualty;<br>grasp back of casualty's wrist;side stroke scissors kick;encourage casualty to stroke with free arm and kick if possiblemaintain observation of casualty.. (Reference from SLSS manual)`);
+}}
