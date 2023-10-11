0b7bab7bb5a60fc65660d154214c1a6151d23a49
RgerWeb <mckeenweb@gmail.com>
2023-10-11 08:02:34 +0800

    f

--- list.js
+++ list.js
@@ -31,4 +31,7 @@ else if (elementId === 's8Resuscitation') {
 else if (elementId === 's3Tow') {
     window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
 }
+else if (elementId === 'accompanied rescue') {
+    alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp aid firmly, kick to move;providing reassurance throughout(SLSS manual)`);
+}
 }
---
+++ list/list1.js
@@ -0,0 +1,15 @@
+function displayAnswer(elementId) {
+    if (elementId === 'accompanied rescue') {
+    alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty; keeping safe distance at defensive position; ask casualty to grasp aid firmly, kick to move;providing reassurance throughout(SLSS manual)`);
+
+} else if (elementId === 'mission') {
+    alert(`to promote lifesaving and water safety`);
+
+} else if (elementId === 'vision') {
+    alert(`to be a leading lifesaving organisation in the world`);
+    
+} else if (elementId === 's3Tow') {
+    window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
+}
+
+}
