51bc941d16964e8d10d0114d43ac7288498642df
RgerWeb <mckeenweb@gmail.com>
2023-10-13 05:50:38 +0800

    g

--- list/list1.js
+++ list/list1.js
@@ -1,36 +1,99 @@
 function displayAnswer(elementId) {
     if (elementId === 'accompanied rescue') {
-    alert(`swim out with a buoyant aid, accompany casualty to safety without direct contact. Approach reassure the casualty;   keeping safe distance at defensive position;   ask casualty to grasp aid firmly, kick to move;  providing reassurance throughout(Reference from SLSS manual)`);  
+    alert(`Accompanied Rescue:
+    -swim out with a buoyant aid and accompany casualty to -safety without direct contact. 
+    -Approach reassure the casualty;   
+    -keeping safe distance at defensive position;   
+    -ask casualty to grasp aid firmly, kick to move;  
+    -provide reassurance throughout.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'non-contact tow') {
-    alert(`swim out with a buoyant aid. Approach reassure the casualty;   keeping safe distance at defensive position;   ask casualty to grasp kickboard using hugging method firmly and float on their back, providing reassurance throughout, hold edge of kickboad and tow casualy prefrebaly with sidestroke scissors kick.(Reference from SLSS manual)`);  
+    alert(`Non-Contact Tow:
+    -Swim out with a buoyant aid. 
+    -Approach reassure the casualty;   
+    -keeping safe distance at defensive position;   
+    -ask casualty to grasp kickboard using hugging method -firmly and float on their back, 
+    -providing reassurance throughout, 
+    -hold edge of kickboad and tow casualy prefrebaly with -sidestroke scissors kick.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'extended tow') {
-    alert(`calm cooperative casualty;   hold casualty's chin, elbows locked straight;   side-stroke scissors kick;   maintain observation of casualty. (Reference from SLSS manual)`);  
+    alert(`Extended Tow:
+    -calm cooperative casualty;   
+    -hold casualty's chin, 
+    -elbows locked straight;   
+    -side-stroke scissors kick;   
+    -maintain observation of casualty. 
+    (Reference from SLSS manual)`);  
     
 } else if (elementId === 'clothing tow') {
-    alert(`calm cooperative clothed casualty;   hold casualty cloths below collar to avoid choking neck, arms relax avoid jerking motion;   egg beater kick or survival kick;   encourage casualty to scull with free arm and kick if possible, maintain observation of casualty. (Reference from SLSS manual)`);  
+    alert(`Clothing Tow:
+    -calm cooperative clothed casualty;
+    -hold casualty cloths below collar to avoid choking neck,
+    -arms relax avoid jerking motion;   
+    -egg beater kick or survival kick;   
+    -encourage casualty to scull with free arm and kick if possible, 
+    -maintain observation of casualty. 
+    (Reference from SLSS manual)`);  
     
 } else if (elementId === 'wrist tow') {
-    alert(`calm cooperative casualty;   grasp back of casualty's wrist;   side stroke scissors kick;   encourage casualty to stroke with free arm and kick if possiblemaintain observation of casualty.. (Reference from SLSS manual)`);  
+    alert(`Wrist Tow:
+    -calm cooperative casualty;   
+    -grasp back of casualty's wrist;   
+    -side stroke scissors kick;   
+    -encourage casualty to stroke with free arm and kick if possible;
+    -maintain observation of casualty.. 
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'chin tow') {
-    alert(`necessaary to maintain firm, close control of casualty;   approach from behind, place one hand on casualty chin;   rest casualty's head on your shoulder on towing arm side;   survival kick, egg beater kick. (Reference from SLSS manual)`);  
+    alert(`Chin Tow:
+    -necessaary to maintain firm, close control of casualty;
+    -approach from behind, place one hand on casualty chin;
+    -rest casualty's head on your shoulder on towing arm side;   
+    -survival kick, egg beater kick(more stable). 
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'double shoulder tow') {
-    alert(`necessaary to maintain greater control, elevate casualty head over rough water;  approach from behind, hook both hands under the armpits and grasp the shoulders;  survival kick, egg beater kick.(Reference from SLSS manual)`);  
+    alert(`Double Shoulder Tow:
+    -necessaary to maintain greater control, 
+    -elevate casualty head over rough water;  
+    -approach from behind, hook both hands under the armpits and grasp the shoulders;  
+    -survival kick, egg beater kick.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'cross chest tow') {
-    alert(`over rough water;  approach from behind, cross an arm over the chest of casualty;  grip under the armpit clamp elbow to casualty's chest;  sidestroke scissors kick.(Reference from SLSS manual)`);  
+    alert(`Cross Chest Tow:
+    -over rough water;  approach from behind, 
+    -cross an arm over the chest of casualty;  
+    -grip under the armpit clamp elbow to casualty's chest;
+    -sidestroke scissors kick.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'support tow') {
-    alert(`with buoyant aid for unconscious non-breathing casualty;  hold board with one hand, chin tow casualty;  every 3 sec, EAR blow to the nose(rest head on board elbow, chin hand press thumb to seal the mouth).CHECK THIS .(Reference from SLSS manual)`);  
+    alert(`Support Tow:
+    -with buoyant aid for unconscious non-breathing casualty;
+    -hold board with one hand, chin tow casualty;  
+    -every 3 sec, EAR blow to the nose(rest head on board elbow, chin hand press thumb to seal the mouth).
+    .(Reference from SLSS manual)`);  
 
 } else if (elementId === 'head tow') {
-    alert(`firm stabilize head hold necessary;  palm inwards place firmly both side of casualty's head;  survial kick or egg beater kick.(Reference from SLSS manual)`);  
+    alert(`Head Tow:
+    -firm & stabilize head hold necessary;
+    -palm inwards place firmly both side of casualty's head;
+    -survial kick or egg beater kick.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'vice grip tow') {
-    alert(`for suspected spinal injury;  approach from side;  place one forearm along spine with hand cupping back of casualty's head;  other forearm on casualty's sternum supporting jaw;  lock both writs and squeeze the forearms together to create a vice, immobilises casualty's body with an upward downward pressure;  for casualty face down in water, vice grip first then rotate person to face up;   move carefully minimise motion altering casualty's hips and legs.(Reference from SLSS manual)`);  
+    alert(`Vice Grip Tow:
+    -for suspected spinal injury;  
+    -approach from side;  
+    -place one forearm along spine with hand cupping back of casualty's head;  
+    -other forearm on casualty's sternum supporting jaw;  
+    -lock both writs and squeeze the forearms together to create a vice, this immobilises casualty's body with an upward downward pressure;  
+    -for casualty face down in water, vice grip first then rotate person to face up;   
+    -move carefully minimise motion altering casualty's hips and legs.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'rescue principles') {
     alert(`Sequence of a lifesaver's rescue steps to be considered a rescue in its entirety: ;  
@@ -42,7 +105,10 @@ function displayAnswer(elementId) {
     [] Aftercare-tel. no. to report casualty's name, hospital sent to.(Reference from SLSS manual)`);  
 
 } else if (elementId === 'rescue steps') {
-    alert(`Also known as "rescue ladder", these are the sequence of rescue to adopt, from the safest for rescuer to risks levels dependent on the rescuer's skill level and competency;  <b>Talk;  Reach ;  Throw;  Wade;  Row;  Swim ;  Tow </b>(non-contact => contact tow);  .(Reference from SLSS manual).(Reference from SLSS manual)`);  
+    alert(`Also known as "rescue ladder", these are the sequence of rescue to adopt, from the safest for rescuer to risks levels dependent on the rescuer's skill level and competency;  
+    Talk - Reach - Throw - Wade - Row - Swim - Tow(non-contact => contact tow);  
+    .(Reference from SLSS manual)`);  
+
 } else if (elementId === 'rescue plan') {
     alert(`A rescuer's plan should consider the following:
     Risks-Talk,Reach,Throw,Wade,Row,Swim,Tow; 
@@ -52,30 +118,72 @@ function displayAnswer(elementId) {
     Rescue aids available; 
     Surroundings; 
     Priorities in this sequence: conscious non-swimmers > conscious swimmer > unconscious casualty > submerged casualty.`);  
+
 } else if (elementId === 'support shallow') {
-    alert(`[] holding one arm under the small of back/the neck of the casualty; OR
-    [] both hands under armpits from behind casualty [] elevate a conscious casualty shoulder level water to help calm him.(Reference from SLSS manual)`);  
+    alert(`Support in Shallow Water:
+    -holding one arm under the small of back/the neck of the casualty; OR
+    -both hands under armpits from behind casualty 
+    -elevate a conscious casualty shoulder level water to help calm him.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'support deep') {
-    alert(`[] from the side; [] holding the neck and supporting the jaw;
-    [] with buoyant aid, rest casualty's neck over elbow joint of the buoyant-aid-holding-arm;
-    [] support the chin with the other hand.(Reference from SLSS manual)`);  
+    alert(`Support in Deep Water:
+    -from the side; 
+    -holding the neck and supporting the jaw;
+    -With buoyant aid, rest casualty's neck over elbow joint of the buoyant-aid-holding-arm;
+    -support the chin with the other hand.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'support position') {
-    alert(`reached point of safety e.g.pool's edge, shore: [] position casualty facing the bank. [] press both arms against casualty's armpits, firm grip on bank support [] one knee may be able to provide additional support. [] casualty head may rest on your shoulder keeping a clear airway.(Reference from SLSS manual)`);  
+    alert(`Support Position:
+    -reached point of safety e.g.pool's edge, river bank,
+    -position casualty facing the bank.
+    -press both arms against casualty's armpits, firm grip on bank support,
+    -one knee may be able to provide additional support.
+    -casualty head may rest on your shoulder keeping a clear airway.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'landing - walk out') {
-    alert(`Gentle slope @ shore, beach, shallow beach.  <b>-walk out,</b>  slide your head under casualty's armpit, put one arm around casualty's waist;  walk beside casualty providing support.`);  
+    alert(`Walk Out:
+    at Gentle slope @ shore, beach, shallow beach.  
+    -slide your head under casualty's armpit, 
+    -put one arm around casualty's waist;  
+    -walk beside casualty providing support.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'landing - pull ashore') {
-    alert(`Gentle slope @ shore, beach, shallow beach.  <b>-drag (pull ashore),</b>  float casualty as close as possible  walk backwards, supporting under the armpits keep mouth clear of water  resuscitation frequent intervals if necessary.`);  
+    alert(`Pull Ashore:
+    at Gentle slope @ shore, beach, shallow beach.  
+    -float casualty as close as possible,
+    -walk backwards, supporting under the armpits keep mouth clear of water,
+    -resuscitation at frequent intervals if necessary.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'assisted carry-lift,lower') {
-    alert(`Gentle slope @ shore, beach, shallow beach.  <b>assisted carry</b>  rescuer one supports casualty under armpits  rescuer two support the legs and carry casualty ashore  lower casualty gently on the ground`);  
+    alert(`Assisted Carry:
+    at Gentle slope @ shore, beach, shallow beach.
+    -rescuer one supports casualty under armpits
+    -rescuer two support the legs and carry casualty ashore 
+    -lower casualty gently on the ground.
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'shoulder carry-lift,lower') {
-    alert(`For gentle slope @ shore, beach, shallow beach.  <b>shoulder carry-lift</b>  over long distance, shallow water, over debris or rough shoreline  for speedy safe exit and leaves one hand free  <b>how</b>-at waist depth water, float casualty face up  grasp wrist of casualty's outer arm using your arm that's facing casualty's feet.  turn towards casualty's feet, lift grasped arm to roll casualty  slide free hand between casualty's legs grasp back of leg above the knee  submerge both shoulders beneath the surface  pull the grasped wrist down,  stand upright using legs, not the back, holding wrist and back of casualty legs while doing it.  transfer wrist to the hand holding back of the leg
-    <b>shoulder carry-lower</b> hold casualty's legs with an arm around thigh and grasp casualty's wrists with the other hand  kneel on knee nearer to casualty's head, keeping back straight doing it  sit casualty on your raised knee  transfer the back leg hand to around the waist  lower casualty to ground sitting position  take the wrist arm around your neck and lower the casualty to lying position from there`);  
+    alert(`Shoulder Carry:
+    For gentle slope @ shore, beach, shallow beach.
+    -over long distance, shallow water, over debris or rough shoreline  for speedy safe exit and leaves one hand free.
+    How:
+    -at waist depth water, float casualty face up,
+    -grasp wrist of casualty's outer arm using your arm that's facing casualty's feet.  
+    -turn towards casualty's feet, lift grasped arm to roll casualty  slide free hand between casualty's legs grasp back of leg above the knee,
+    -submerge both shoulders beneath the surface pull the grasped wrist down, stand upright using legs, not the back, holding wrist and back of casualty legs while doing it.  
+    -transfer wrist to the hand holding back of the leg.
+    Lower:
+    -hold casualty's legs with an arm around thigh and grasp casualty's wrists with the other hand  
+    -kneel on knee nearer to casualty's head, keeping back straight doing it  
+    -sit casualty on your raised knee transfer the back leg hand to around the waist  
+    -lower casualty to ground sitting position  
+    -take the wrist arm around your neck and lower the casualty to lying position from there
+    (Reference from SLSS manual)`);  
 
 } else if (elementId === 'pick a back carry-lift,lower') {
     alert(`<b>lift</b> stand in chest depth water  float casualty in front of you facing you  grasp casulty's left wrist with your left hand, right wrist with your right hand  turn sharply around under the far arm and pull casualty's chest onto your shoulders  wade ashore leaning forward from the hips, supporting casualty's weight across both shoulders
