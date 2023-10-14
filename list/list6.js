a55951a352c99ecb55b089bd47c951ee95f8b6ea
RgerWeb <mckeenweb@gmail.com>
2023-10-14 15:38:20 +0800

    t

--- list/list6.js
+++ list/list6.js
@@ -3,36 +3,40 @@ function displayAnswer(elementId) {
 
     //s3Tow.html*    
     
-        if (elementId === 'accompanied rescue') {
-        alert(`Accompanied Rescue:
-        -swim out with a buoyant aid and accompany casualty to -safety without direct contact. 
-        -Approach reassure the casualty;   
-        -keeping safe distance at defensive position;   
-        -ask casualty to grasp aid firmly, kick to move;  
-        -provide reassurance throughout.
-        (Reference from SLSS manual)`);  
+        if (elementId === 'respiratory intro') {
+        alert(`We are respiratory organisms, every cell in the body needs oxygen for survival.  Our respiratory system consists of all organs, tissues and structures that facilitates breathing.  Respiratory failures or malfunctions are considered emergencies.`);  
         
-} else if (elementId === '') {
-    alert(`.(References from SLSS manual)`);
+} else if (elementId === 'pharynx') {
+    alert(`*Pharynx*
+    A short path for air & food leading to two tubes: gullet (oesophagus) for food and fluid to the stomach, windpipe (trachea) for air to the lungs.(References from SLSS manual)`);
+
+} else if (elementId === 'larynx') {
+    alert(`*Larynx*
+    Voice box(larynx) at the top of the trachea, has a muscular valve (epiglottis) that prevents food entering the larynx.  Larynx lies in a strong cartilage box (adam's apple).
+
+    Malfunctions: epiglottis may fail to function in unconscious/semi-conscious persons. 
+
+    Damage: epiglootis failure means air cannot effectively enter the lungs, food or fluid may enter the lungs, highly irritating to the lungs, inhalation of vomit is often fatal.(References from SLSS manual)`);
+
+} else if (elementId === 'bronchi') {
+    alert(`*Bronchi*
+    The trachea divides in two tubes(bronchi) into each lung.  The main bronchi divides into smaller tubes (bronchioles).  Both the trachea and bronchi, bronchioles are strengthened by U-shaped cartilage rings(References from SLSS manual)`);
+
+} else if (elementId === 'alveoli') {
+    alert(`*alveoli*
+    End of bronchioles as air sacs(alveoli), amounting to 300 million alveoli equivalent to area of a tennis court.(References from SLSS manual)`);
+
+} else if (elementId === 'cilia') {
+    alert(`*cilia*
+    Cells covered with tiny hairs(cilia) that lined the trachea and bronchi, kept moist by glands.  Cilia trap particles and coughed out as phlegm(sputum).
+    (References from SLSS manual)`);
+
+} else if (elementId === 'gas exchange') {
+    alert(`The oxygen breathed in, passes thin walls of the alveoli.(References from SLSS manual)`);
 
 } else if (elementId === '') {
     alert(`.(References from SLSS manual)`);
 
-} else if (elementId === '') {
-    alert(`.(References from SLSS manual)`);
-
-} else if (elementId === '') {
-    alert(`.(References from SLSS manual)`);
-
-} else if (elementId === '') {
-    alert(`.(References from SLSS manual)`);
-
-} else if (elementId === '') {
-    alert(`.(References from SLSS manual)`);
-
-} else if (elementId === '') {
-    alert(`.(References from SLSS manual)`);
-    
 } else if (elementId === '') {
     alert(`.(References from SLSS manual)`);
 
