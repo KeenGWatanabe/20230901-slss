74e32eca12eecf176c2ba961f857b2918dba6ef1
RgerWeb <mckeenweb@gmail.com>
2023-10-15 14:16:54 +0800

    t

--- list.js
+++ list.js
@@ -20,11 +20,29 @@ else if (elementId === 'contents') {
 else if (elementId === 'disclaimer') {
     alert(`This content is created for trainee and trained lifesaver's references only.  Context of saving methodology is subject to skills and experiences interpretation, quotes here are by no means comprehensive enough to be used alone. (Web Developer)`);
 }
+else if (elementId === 's2Hygiene') {
+    window.open(href="list/s2Hygiene.html",'chat AI','resizable,height=300,width=500');
+
+}
 else if (elementId === 's3Rescue') {
     window.open(href="list/s3Rescue.html",'chat AI','resizable,height=300,width=500');
 }
+else if (elementId === 's3Tow') {
+    window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
+}
 else if (elementId === 's4Resuscitation') {
     window.open(href="list/s4Resuscitation.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's6CardiovascularSys') {
+    window.open(href="list/s6CardiovascularSys.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's6RespiratoryIntro') {
+    window.open(href="list/s6RespiratorySys.html",'chat AI','resizable,height=300,width=500');
+}
+
+else if (elementId === 's6Spinal') {
+    window.open(href="list/s6Spinal.html",'chat AI','resizable,height=300,width=500');
+
 }
 else if (elementId === 's8Rescue') {
     window.open(href="list/s8Rescue.html",'chat AI','resizable,height=300,width=500');
@@ -32,17 +50,8 @@ else if (elementId === 's8Rescue') {
 else if (elementId === 's8Resuscitation') {
     window.open(href="list/s8Resuscitation.html",'chat AI','resizable,height=300,width=500');
 }    
-else if (elementId === 's3Tow') {
-    window.open(href="list/s3Tow.html",'chat AI','resizable,height=300,width=500');
-}
-else if (elementId === 's6RespiratoryIntro') {
-    window.open(href="list/s6RespiratorySys.html",'chat AI','resizable,height=300,width=500');
-}
-else if (elementId === 's6CardiovascularSys') {
-    window.open(href="list/s6CardiovascularSys.html",'chat AI','resizable,height=300,width=500');
-}
-else if (elementId === 's6Spinal') {
-    window.open(href="list/s6Spinal.html",'chat AI','resizable,height=300,width=500');
 
-}
+
+
+
 }
