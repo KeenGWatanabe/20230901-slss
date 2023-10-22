02af235f413edc9522d2ed855686203c79b5887c
60936 <studio34pl@gmail.com>
2023-10-22 19:29:48 +0800

    1

--- list/list5.js
+++ list/list5.js
@@ -134,7 +134,7 @@ function displayAnswer(elementId) {
 
 
 } else if (elementId === 'vice grip video') {
-    window.open(href="../videos/ViceGripVideo.mp4",'chat AI','resizable,height=300,width=500');
+    window.open(href="../videos/",'chat AI','resizable,height=300,width=500');
 //https://youtu.be/z6-UhOZ6X1A?si=gmyPqopHQX4-Wlyk
 } else if (elementId === 'extended arm rollover video') {
     window.open(href="https://youtu.be/1ANZhOqZs9w?si=Ha77cKpOg8I-1DUN",'chat AI','resizable,height=300,width=500');
